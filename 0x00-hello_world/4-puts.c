6825716ce44a57e2814662ab50896e7cc7a8c3c0
Sweilam <boodyhussein12@gmail.com>
2023-08-10 18:56:56 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-int main ()
+/**
+ * main prints "Programming is like building a multilingual puzzle")
+ * Returns 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 
 
 
