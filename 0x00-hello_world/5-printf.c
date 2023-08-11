11bcebe4f5183a949737902257f7c8d2523ac625
Sweilam <boodyhussein12@gmail.com>
2023-08-10 19:09:56 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,18 @@
 /*
- * FILE : 5-printf.c
- * Auth : Abdelrahman Sweilam
- */
+* FILE : 5-printf.c
+* Auth : Abdelrahman Sweilam
+*/
 
 #include<stdio.h>
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
- *
- * Return: Always 0.
- */
+* main - Prints "with proper grammar, but the outcome is
+*                a piece of art," followed by a new line.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-  return (0);
+ return (0);
 }
