999c21c348da7d429ba17414da886d42e23c9ead
Sweilam <boodyhussein12@gmail.com>
2023-08-18 10:24:17 -0700

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,11 +16,9 @@ if (size > 0){
 for (hgt = 0; hgt < size; hgt++){
 for (wid = 0; wid < size; wid++)
 _putchar('#');
-
 if (hgt == size - 1)
 continue;
-_putchar('\n');
-}
+_putchar('\n'); }
 }
 _putchar('\n');
 }
