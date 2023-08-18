0d2eb18077e29c78ebc9c1973c0263d37b32ac4c
Sweilam <boodyhussein12@gmail.com>
2023-08-18 11:00:42 -0700

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,13 @@
  * print_square - Prints a squareusing the character #.
  * @size: The size of the square.
  */
-void print_square(int size){
+void print_square(int size)
+{
 int hgt, wid;
-if (size > 0){
-for (hgt = 0; hgt < size; hgt++){
+if (size > 0)
+{
+for (hgt = 0; hgt < size; hgt++)
+{
 for (wid = 0; wid < size; wid++)
 _putchar('#');
 if (hgt == size - 1)
@@ -21,6 +24,3 @@ _putchar('\n'); }
 }
 _putchar('\n');
 }
- 
-
-  
