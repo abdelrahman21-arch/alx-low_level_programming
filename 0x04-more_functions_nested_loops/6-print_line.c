d03f3be62d45506c3768280b3bb517684069d9d4
Sweilam <boodyhussein12@gmail.com>
2023-08-18 08:54:20 -0700

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,11 +4,10 @@
 */
 
 #include "main.h"
-
-	/**
-	 * print_line - Draws a straight line using the character _.
-	 * @n: The number of _ characters to be printed.
-	 */
+/**
+* print_line - Draws a straight line using the character _.
+* @n: The number of _ characters to be printed.
+*/
 void print_line(int n)
 {
 int len;
