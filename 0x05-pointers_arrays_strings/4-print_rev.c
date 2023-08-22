dc314ba5229c5e65d37e138f7ebcc8e35cdca0aa
Sweilam <boodyhussein12@gmail.com>
2023-08-22 16:50:02 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,10 +14,10 @@ void print_rev(char *s)
 int len = 0, index;
 
 while (s[index++])
- len++;
+len++;
 
 for (index = len - 1; index >= 0; index--)
- _putchar(s[index]);
+_putchar(s[index]);
 
- _putchar('\n');
+_putchar('\n');
 }
