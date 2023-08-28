4f432b45c3e9cfb6862fb936f8e86af7e6028f6e
abdelrahman21-arch <61055709+abdelrahman21-arch@users.noreply.github.com>
2023-08-29 00:42:54 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
 */
 #include "main.h"
 /**
-* memset - Fills the first n bytes of the memory area
+* _memset - Fills the first n bytes of the memory area
 *          pointed to by @s with the constant byte @b.
 * @s: A pointer to the memory area to be filled.
 * @b: The character to fill the memory area with.
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int index;
 char *memory = s , value = b;
-for(index = 0; index<n; index++)
+for(index = 0; index < n; index++)
 {
 memory[index] = value;
 }
