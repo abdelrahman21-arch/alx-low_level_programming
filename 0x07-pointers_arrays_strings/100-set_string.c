bcae33e3c549ccc2589e2e31b97d48fdd8e82e98
sweilam <boodyhussein12@gmail.com>
2023-08-29 15:30:34 -0700

    update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+/*
+ * file: 9-set_string.c
+ * auth: Sweilam
+ */
+
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char
+ * @s: The pointer.
+ * @to: The char.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
