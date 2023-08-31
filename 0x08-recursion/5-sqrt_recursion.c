deb0300b4b6bda6f3acf1feed7c1ec4d3163f675
sweilam <boodyhussein12@gmail.com>
2023-08-31 16:50:56 -0700

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,13 +18,12 @@ int _sqrt_recursion(int n);
  */
 int find_sqrt(int num, int root)
 {
-  if ((root * root) == num)
-    return (root);
+if ((root * root) == num)
+return (root);
+if (root == num / 2)
+return (-1);
 
-  if (root == num / 2)
-    return (-1);
-
-  return (find_sqrt(num, root + 1));
+return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -36,13 +35,11 @@ int find_sqrt(int num, int root)
  */
 int _sqrt_recursion(int n)
 {
-  int root = 0;
+int root = 0;
 
-  if (n < 0)
-    return (-1);
-
-  if (n == 1)
-    return (1);
-
-  return (find_sqrt(n, root));
+if (n < 0)
+return (-1);
+if (n == 1)
+return (1);
+return (find_sqrt(n, root));
 }
