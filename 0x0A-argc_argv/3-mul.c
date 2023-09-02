0b190a729a1cf9fe4826d2fafc5819174fa9eea1
sweilam <boodyhussein12@gmail.com>
2023-09-01 17:42:41 -0700

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+/*
+ * file : 0-whatsmyname.c
+ * Author: Sweilam
+ */
+
+/**
+ * main -  program entry point
+ * @argc: command line arguments count
+ * @argv: array of strings of command line arguments
+ * Return: 0 on success
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[] )
+{
+int x, y, prod;
+if(argc != 3 )
+{
+exit (EXIT_FAILURE);
+}
+x = atoi (argv[1] );
+y = atoi (argv[2] );
+prod = x * y;
+printf ("%d\n", prod);
