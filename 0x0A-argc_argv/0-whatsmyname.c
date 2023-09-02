a83e31b6d34d0345cd1f2530913c1f3a2c15e02f
sweilam <boodyhussein12@gmail.com>
2023-09-01 17:12:38 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,6 +7,7 @@
  * main -  a program that prints its name, followed by a new line.
  * @argc: command line arguments count
  * @argv: array of strings of command line arguments
+ * Return: 0 on success
  */
 #include <stdio.h>
 #include <stdlib.h>
