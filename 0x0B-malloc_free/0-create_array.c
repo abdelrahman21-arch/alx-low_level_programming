c18dd38fdcdf04a7bdd899dae70e845596b5996d
sweilam <boodyhussein12@gmail.com>
2023-09-05 16:11:17 -0700

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-create_array.c
+ * Auth: Sweilam
+ */
+
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array -  creates an array of chars
+ *                  , and initializes it with a specific char
+ * @size: The size of the array to be initialized
+ * @c: The specific char to initialize the array with.
+ * Return: if size == 0 or the function fails - Null
+ */
+
+char *create_array(unsigned int size , char c ){
+
+char *array;
+unsigned int index;
+if (size == 0)
+return (NULL);
+array = malloc(sizeof(char) *size);
+if (array == NULL)
+return (NULL);
+for (index = 0; index < size; index++)
+array[index] = c ;
+return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+char *create_array(unsigned int size, char c);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
