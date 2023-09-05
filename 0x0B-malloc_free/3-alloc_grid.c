e0e4fefb17810032f9b001fd1ff2a632e2e69299
sweilam <boodyhussein12@gmail.com>
2023-09-05 16:36:29 -0700

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,7 +39,7 @@ return (NULL);
 for (hgt_index = 0; hgt_index < height; hgt_index++)
 {
 for (wid_index = 0; wid_index < width; wid_index++)
-woD[hgt_index][wid_index] = 0;
+twoD[hgt_index][wid_index] = 0;
 }
 return (twoD);
 }
