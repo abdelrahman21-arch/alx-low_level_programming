5f2bba44fc87dab76bf568f00dbbcb4cef510700
sweilam <boodyhussein12@gmail.com>
2023-09-06 14:02:56 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int num, digit, sum = 0;
 for (num = 1; num < argc; num++)
 {
 for (digit = 0; argv[num][digit]; digit++)
-{	
+{
 if (argv[num][digit] < '0' || argv[num][digit] > '9')
 {
 printf("Error\n");
